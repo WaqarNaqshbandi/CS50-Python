6ddba3e930e34a72262b2c645112af0640b0ff87
Waqar Naqshbandi <heartlesscyborgs@gmail.com>
2024-08-18 16:18:54 +0500

    learnt integers

---
+++ int.py
@@ -0,0 +1,15 @@
+# Not a real calculator yet.
+#Use int function to convert your strings to integers
+
+"""x = input("What's x? ")
+y = input("What's y? ")
+
+z = int(x) + int(y)
+
+print(z)"""
+
+# You can nest functions like this
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+print(x + y)
--- string-methods.py
+++ string-methods.py
@@ -7,7 +7,11 @@ name = name.strip()
 # Capitalize, only capitalizes the first word.
 name = name.capitalize()
 
-# Titlecase, capitalizes every word
-name = name.title()"""
+Titlecase, capitalizes every word
+name = name.title() 
+
+Split
+first, last = name.split(" ") The space in quotes means, we split wherever space comes in. first, last now become new variables taken out of the name after it's split.
+"""
 
 print(f"hello, {name}")
