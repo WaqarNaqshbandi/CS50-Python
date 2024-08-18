84c0e0f6083a7ce9a4c32bf20b888fb78fbeceab
Waqar Naqshbandi <heartlesscyborgs@gmail.com>
2024-08-18 09:41:03 +0500

    learnt string-methods

---
+++ string-methods.py
@@ -0,0 +1,13 @@
+# Chain functions / methods
+name = input("What's your name? ").strip().capitalize().title()
+
+""" Remove spaces, use strip function/method for a string
+name = name.strip()
+
+# Capitalize, only capitalizes the first word.
+name = name.capitalize()
+
+# Titlecase, capitalizes every word
+name = name.title()"""
+
+print(f"hello, {name}")
